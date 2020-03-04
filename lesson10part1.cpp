317d0513e27685cd8e25dd5ec4f1056df2d91d10
SRS-boop <58885260+SRS-boop@users.noreply.github.com>
2020-03-03 18:30:38 -0600

    Add files via upload

---
+++ lesson10part1.cpp
@@ -0,0 +1,174 @@
+/*
+Name: Samantha Sax
+NetID: srs190010
+
+This function reads in a students' and the correct quiz answers from input files and stores that data into two parallel arrays.
+The data in the arrays are then compared for parity, and the score of the student is calculated and recorded. Incorrect student 
+answers are displayed with their correct answers.*/
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <array>
+using namespace std;
+
+
+//prototype functions
+void displayResults(char studentAnswers[], char correctAnswers[], int countCorrectAns);
+int readAnswers(char studentAnswers[], string, int& count);
+
+
+//main function 
+int main()
+{
+	//define local variables/arrays
+	char correctAnswers[30];
+	char studentAnswers[30];
+	char tempArr[30];
+	int size;
+	string studentInput;
+	string correctInput;
+
+
+	//get user input
+	cout << "Enter student answers file name\n";
+	getline(cin, studentInput);
+
+	//call read funtion and assign the return value to a variable that will be used to record array size
+	int size1 = readAnswers(tempArr, studentInput, size);
+
+
+	//transfer values from tempArr to student answer array
+	for (int i = 0; i < size1; i++) {
+		studentAnswers[i] = tempArr[i];
+	}
+
+	//if the previous read worked, repeat the process with the correct answer input file file 
+	if (size1 != -1) {
+		cout << "Enter correct answer file name\n";
+		getline(cin, correctInput);
+
+		//call function and record return value
+		int size2 = readAnswers(tempArr, correctInput, size);
+
+
+		//if second read failed, return error message
+		if (size2 == -1) {
+			cout << "File \"" << correctInput << "\" could not be opened\n";
+		}
+
+		//if it didnt fail, assign correct answer array with tempArr values
+		else {
+
+
+			for (int i = 0; i < size2; i++) {
+				correctAnswers[i] = tempArr[i];
+			}
+
+			//assign number of correct answers to count correct variable
+			//int countCorrectAns = size;
+
+			//compare the two count variables and display errors if needed
+			if (size1 != size2) {
+				cout << "The student answers file has " << size1 << " entries and the correct answers file has " << size2 << " entries\n";
+				cout << "Grading cannot be done if they are not the same\n";
+
+				exit(EXIT_FAILURE);
+			}
+
+			//if theyre both zero
+			else if ((size1 == 0) && (size2 == 0)) {
+				cout << "The number of student answers and correct answers are both 0\n";
+				cout << "No grade can be calculated\n";
+
+				exit(EXIT_FAILURE);
+			}
+			//the else statement will call the display function 					
+			else {
+				
+				displayResults(studentAnswers, correctAnswers, size);
+
+			}
+		}
+	}
+	else {
+		cout << "File \"" << studentInput << "\" could not be opened\n";
+	}
+}
+
+
+
+
+//function definitions
+//the readAnswers function opens an input file specified by the user, reads the data into an array, and returns the size of the array.
+			int readAnswers(char tempArr[], string inputFileName, int& size) {
+				ifstream inputFile;
+				inputFile.open(inputFileName);
+				if (inputFile.is_open()) {
+
+
+					//define variable to store the read-in data
+					char answer;
+					size = 0;//keep track of number of elements in array
+					for (int i = 0; i < 30; i++) {
+						if (inputFile >> answer) {
+							tempArr[i] = answer;
+							size = size + 1;
+						}
+							else { break; }
+						}
+
+					//if array elements exceed 30, rturn 30
+					if (size > 30) {
+						return 30;
+					}
+						//else if they don't exceed 30, return the number of elements
+						else if (size <= 30) {
+							return size;
+						}
+				}	//else if the file fails to open, display error message.
+						else if (inputFile.fail()) {
+					//cout << "File \"" << inputFileName << "\" could not be opened\n";
+						return -1;
+				}
+			}
+		
+			//this function calculates and displays the results of the student's score based on passed in values.
+			void displayResults(char studentAnswers[], char correctAnswers[], int size) {
+				
+				//define and initialize counters for the number of correct, incorrect, and total answers.
+				int correctAns = 0;
+				int incorrectAns = 0;
+				int totalAns = size; //to use this function, returnVal1 and returnVal2 must be equal so i could have used either here.
+
+				//iterate through the arrays and compare values of their contents
+				for (int i = 0; i < size; i++) {
+					if (studentAnswers[i] == correctAnswers[i]) {
+						continue;
+						
+					}
+						else {
+							cout << "Question " << (i + 1) << " has incorrect answer \'" << studentAnswers[i] << "\', the correct answer is \'" << correctAnswers[i] << "\'\n";
+							incorrectAns = incorrectAns + 1;
+						}
+				}
+				
+				//calculate number of correct answers and score
+				correctAns = totalAns - incorrectAns;
+				//cast to doubles for calculations
+				double score = (static_cast<double>(correctAns) / static_cast<double>(totalAns)) * 100;
+				
+				//display results to the console.
+				cout << setprecision(1) << fixed;
+				cout << incorrectAns << " questions were missed out of " << totalAns << endl;
+				cout << "The student grade is " << score << "%\n";
+				if ((score - 70) < -0.001) {
+					cout << "The student failed\n";
+				}
+
+					else {
+						cout << "The student passed\n";
+					}
+			}
---
+++ lesson10part2.cpp
@@ -0,0 +1,169 @@
+/*
+Name: Samantha Sax
+NetID: srs190010
+
+
+This program will read in a file containing five columns and a maximum of 30 rows of grades, store those values in 
+a multidimentional array, and calculate the average, and smallest values of those grades per column and row respectively.*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const int MAX_COLUMNS = 5;
+
+
+// read input file and populate the array
+int readFile(double values[][MAX_COLUMNS], int maxRows, string inputFileName);
+
+// for the array
+double average(double values[][MAX_COLUMNS], int numberRows);
+
+// for a specified column (column)
+double columnAverage(double values[][MAX_COLUMNS], int column, int numberRows);
+
+//smallest values in a row function prototype
+double smallestRowValue(double values[][MAX_COLUMNS], int rowNumber);
+
+
+
+//MAIN FUNCTION BEGINS HERE
+int main()
+{
+	const int maxRows = 30;//constant max rows 
+	double values[maxRows][MAX_COLUMNS];
+	string inputFileName;
+	
+
+	//get user-defined input file name
+	cout << "Enter input file name\n";
+	  getline(cin, inputFileName);
+
+	  //call the funtion to read, store in an array, and return the number of rows read in
+	  int actualRows = readFile(values, maxRows, inputFileName);
+	
+	  //if the file couldn't be read, display error message
+	  if (actualRows == -1) {
+		  cout << "File \"" << inputFileName << "\" could not be opened\n";
+		  exit(EXIT_FAILURE);//quit the program
+	  }
+
+		//if the file was empty
+		 else if (actualRows == 0) {
+			  cout << "The input file \"" << inputFileName << "\" did not contain any data\n";
+		  }
+
+			//process the data
+			else {
+				cout << "Processing " << actualRows << " rows, and 5 columns\n";
+				cout << "Average for all values is " << setprecision(2) << fixed << average(values, actualRows) << endl;
+				
+				//loop through each column to display the average of each
+				for (int colNum = 0; colNum < MAX_COLUMNS; colNum++) {
+					  cout << "Average for column " << colNum << " is " << columnAverage(values, colNum, actualRows) << endl;
+				}
+				//loop through rows to display the smallest values of each
+				for (int rowNum = 0; rowNum < actualRows; rowNum++) {
+					 cout << "Smallest value for row " << rowNum << " is " << smallestRowValue(values, rowNum) << endl;
+				}
+			}	 
+}
+
+//FUNCTION DEFINITIONS
+
+//inputFile will have 5 columns and a maximum of 30 rows
+int readFile(double values[][MAX_COLUMNS], int maxRows, string inputFileName) {
+	
+	double val1;//variable that stores the data point read in from the file
+	int rowCounter = 0;//keep track of how many rows are read in
+
+	//open input file
+	ifstream inputFile;
+	inputFile.open(inputFileName);
+
+	//verify the file is open
+	if (inputFile.is_open()) {
+
+		//if it is open, check to make sure it is not empty by checking for the End Of File marker
+		if (inputFile.peek() == EOF) {
+			return 0;//if it is empty, return a 0
+		}
+
+			//if it is not empty, iterate through the columns and rows, extracting a data point at a time
+			for (int rowNum = 0; rowNum < maxRows; rowNum++) {
+				for (int colNum = 0; colNum < MAX_COLUMNS; colNum++) {
+					
+					if ((inputFile.peek() == EOF)) {//if this row is empty, return total rows read in 
+					
+						return rowCounter;
+					}
+						else {//if this row is not empty, read in and assign the data point to the array
+							inputFile >> val1;
+							values[rowNum][colNum] = val1;
+						
+						
+						}
+				}
+					
+			rowCounter++;//increment the row counter and go back through the first for loop.
+				}
+			return rowCounter;
+
+	}
+	
+	//if the file failed to open, return -1	
+	else if (inputFile.fail()) {
+		return -1;
+	}
+}
+
+//average for all values function begins here
+double average(double values[][MAX_COLUMNS], int numberRows) {
+	double total = 0;//initialize total to 0
+	for (int rowNum = 0; rowNum < numberRows; rowNum++) {
+		for (int colNum = 0; colNum < MAX_COLUMNS; colNum++) {
+			total = total + values[rowNum][colNum];//total is reassigned to itself plus the stored value
+		}
+	}
+
+	//calculate average from sum
+	double average = (total / (static_cast<double>(numberRows) * 5.0));//cast to a double to avoid truncation
+
+		//return the average calculated
+		return average;
+}
+
+//column average calculation function begins here
+double columnAverage(double values[][MAX_COLUMNS], int column, int numberRows) {
+	//specific column designation is passed in by value
+	double columnTotal = 0;//initialize columnTotal sum to 0 
+	double averageOfOneCol;
+	
+	for (int rowNum = 0; rowNum < numberRows; rowNum++) {
+		columnTotal = columnTotal + values[rowNum][column];//add the columnTotal to the next value in the column
+	}
+	//calculate average, casting ints to doubles to avoid truncation
+	averageOfOneCol = (columnTotal / static_cast<double>(numberRows));
+	
+	//return average of the specified column
+		return averageOfOneCol;
+}
+
+//smallest value per row function begins here
+double smallestRowValue(double values[][MAX_COLUMNS], int rowNumber) {
+	//specified row to be calculated is passed in by value
+	double smallestVal = values[rowNumber][0];//set the smallest value to the first value in that row (value at column 0)
+
+	for (int colNum = 0; colNum < MAX_COLUMNS; colNum++) {
+		if (values[rowNumber][colNum] <= smallestVal){//if the next value is smaller than the current smallest value, reassign 
+			smallestVal = values[rowNumber][colNum];
+		}
+	}
+
+	//return the smallest value in that row
+		return smallestVal;
+}
+
